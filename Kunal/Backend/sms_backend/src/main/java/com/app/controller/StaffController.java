f30ab691c7349967d77170c8a3a289c379a54fa3
Kunal <kunalpawar0296@gmail.com>
2024-02-22 10:43:51 +0530

    Backend updated

--- Kunal/Backend/sms_backend/src/main/java/com/app/controller/StaffController.java
+++ Kunal/Backend/sms_backend/src/main/java/com/app/controller/StaffController.java
@@ -1,6 +1,7 @@
 package com.app.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotNull;
@@ -9,6 +10,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +20,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.dto.attendance.AttendDto;
 import com.app.dto.staff.StaffDto;
+import com.app.dto.staff.StaffLoginDto;
 import com.app.entities.primary.Staff;
 import com.app.service.StaffService;
 import com.app.util.CreatePayload;
@@ -27,6 +29,7 @@ import com.app.util.ResponseText;
 
 @RestController
 @RequestMapping("/staff")
+@CrossOrigin("http://localhost:3000/")
 public class StaffController {
 	@Autowired
 	private ModelMapper mapper;
@@ -38,6 +41,19 @@ public class StaffController {
 	{
 		return  ResponseEntity.status(HttpStatus.OK)
 				.body(new CreatePayload<StaffDto>("Staff list",staffService.getStaffList())); 
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> authenticateStaff(@RequestBody StaffLoginDto staffDto)
+	{
+		try {
+			return ResponseEntity.status(HttpStatus.OK)
+					.body(staffService.authenticateStaff(staffDto));
+		}
+		catch(NoSuchElementException e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+					.body("Invalid credentials");
+		}
 	}
 	
 	@PostMapping
